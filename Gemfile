4edcff692bbc0639fe4969ebd8be139282e96dc1
Joe Yates <joe.g.yates@gmail.com>
2011-03-12 23:02:32 +0000

    Started adding tests

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.5'
 gem 'sqlite3'
 gem 'haml'
+
+group :development do
+  gem 'rspec',                 '2.5.0'
+  gem 'rspec-rails',           '2.5.0'
+end
+
---
+++ spec/models/node_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname( __FILE__ ) + '/../spec_helper'
+
+describe Node do
+
+  it "should parse chunks" do
+    pending
+  end
+
+  it "should raise an error when the root line is not found" do
+    pending
+  end
+
+  it "should have a parent" do
+    pending
+  end
+
+  it "should have children" do
+    pending
+  end
+
+  it "should allow parent to be missing" do
+    pending
+  end
+
+  it "should" do
+    pending
+  end
+
+  it "should supply info" do
+    pending
+  end
+
+end
